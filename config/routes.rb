34eb9ef0d5a70dec49a8fa72a8683ed656c5b6ce
titanium-cranium <titaniumcranium@icloud.com>
2015-10-20 10:02:29 +0000

    generated tours controller, created root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Curves::Application.routes.draw do
+  root 'tours#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
