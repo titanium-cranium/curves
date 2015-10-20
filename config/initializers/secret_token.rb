69a891201e75f0d97bc578c28566e3b0661f00a4
titanium-cranium <titaniumcranium@icloud.com>
2015-10-20 02:22:56 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Curves::Application.config.secret_key_base = 'd7941226da10e3f851731e19389c9ece21a4b7f665ce4ec548e8d87fd518b046c1d7652d0406b2f8aee10dbd798c25d4a4419cbc9eeaddd22b419d4142b01069'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Curves::Application.config.session_store :cookie_store, key: '_curves_session'
