045b9f556e4a009ea1197cc172efc0ba8f5ee357
titanium-cranium <titaniumcranium@icloud.com>
2015-10-31 00:42:47 +0000

    pagination in footer failed.  Had to reformat to put back onto index page only.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Curves::Application.routes.draw do
   root 'tours#index'
+  resources :tours
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
