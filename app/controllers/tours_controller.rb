82cbb43a29f79eb768ec77fed0095416c2a39956
titanium-cranium <titaniumcranium@icloud.com>
2015-11-03 00:32:59 +0000

    Lesson14 adding form functionality

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -6,4 +6,14 @@ class ToursController < ApplicationController
 	def new
 		@tour = Tour.new
 	end
+
+	def create
+		Tour.create(tour_params)
+	end
+
+	def tour_params
+		Params.require(:tour).permit(:name, :description, :start_location, :finish_location, :state, :country, :difficulty, :surface, :distance)
+	end
+
+
 end
