0e18c4282a02c3abc2432af4f44165c720a5590b
titanium-cranium <titaniumcranium@icloud.com>
2015-11-10 01:31:16 +0000

    Added security protocols to prevent editing, deleting or updating by modifying authentication and controller methods

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -1,5 +1,5 @@
 class ToursController < ApplicationController
-	before_action :authenticate_user!, :only => [:new, :create]
+	before_action :authenticate_user!, :only => [:new, :create, :edit]
 
 	def index
 		@tours = Tour.all.order('created_at ASC').page(params[:page]).per(4)
@@ -20,10 +20,16 @@ class ToursController < ApplicationController
 
 	def edit
 		@tour = Tour.find(params[:id])
+		if @tour.user != current_user
+			return render :text => 'Editing tour details restricted to the creator.', :status => :forbidden
+		end
 	end
 
 	def update
 		@tour = Tour.find(params[:id])
+			if @tour.user != current_user
+				return render :text => 'Editing tour details restricted to the creator.', :status => :forbidden
+			end
 		@tour.update_attributes(tour_params)
 		redirect_to tour_path
 	end
