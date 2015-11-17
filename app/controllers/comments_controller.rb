e9e33529b0d6313bc580546234153652af8ed7e6
titanium-cranium <titaniumcranium@icloud.com>
2015-11-17 23:55:25 +0000

    defined create method in comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,18 @@
 class CommentsController < ApplicationController
-	before_action :authenticate_user!, :only => [:new, :create, :edit, :destroy]
+	before_action :authenticate_user!
 
-# def index
-#   @comments = Comment.all.order(created_at 'ASC')
-# end
-#
-#
-#   def new
-#     @comment = Comment.new.tour_id
-#   end
-#
+	def create
+		@tour = Tour.find(params[:tour_id])  #this pulls the tour the comment belongs to from the url
+		@tour.comments.create(comment_params.merge(:user => current_user))
+		redirect_to tour_path(@tour)
+	end
+
+
+private
+
+	def comment_params
+		params.require(:comment).permit(:message, :rating)
+	end
+
+
 end
