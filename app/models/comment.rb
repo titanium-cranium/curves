e847002461ff28158579b163705fbd06aad5e5c9
titanium-cranium <titaniumcranium@icloud.com>
2015-11-18 01:08:58 +0000

    changed rating field to drop-down list, humanized the display

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,17 @@
 class Comment < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :tour
+
+	RATINGS = {
+		'one star'		=> '1_star',
+		'two star'		=> '2_star',
+		'three star'	=> '3_star',
+		'four star'		=> '4_star',
+		'five star'		=> '5_star',
+	}
+
+	def humanized_rating
+		RATINGS.invert[self.rating]
+	end
+
 end
