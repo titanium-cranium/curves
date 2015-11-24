b71bbea2eb52b6e2e665b8317891fe306e402d3e
titanium-cranium <titaniumcranium@icloud.com>
2015-11-24 03:21:19 +0000

    Setup gmail test account, added functionality to mailer from there

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,6 +2,6 @@ class NotificationMailer < ActionMailer::Base
   default from: "no-replay@kneescrapers.com"
 
   def comment_added
-    mail(to: "b.obrien@me.com", subject: "A comment has been added to the tour you created")
+    mail(to: "b.obrien@me.com", subject: "A comment has been added to the tour you created.")
   end
 end
