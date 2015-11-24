4716582924d411a2d45c2869a4582b0875da68e8
titanium-cranium <titaniumcranium@icloud.com>
2015-11-24 02:21:15 +0000

    Added NotificationMailer, configured comment_added autoresponse

---
+++ app/mailers/notification_mailer.rb
@@ -0,0 +1,7 @@
+class NotificationMailer < ActionMailer::Base
+  default from: "no-replay@kneescrapers.com"
+
+  def comment_added
+    mail(to: "b.obrien@me.com", subject: "A comment has been added to the tour you created")
+  end
+end
