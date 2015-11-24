2f029ea54645986d4aaebdc3c1d76eec4b5f0f64
titanium-cranium <titaniumcranium@icloud.com>
2015-11-24 04:04:15 +0000

    Added tour and email variables to comment mailer

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,7 +1,9 @@
 class NotificationMailer < ActionMailer::Base
   default from: "no-replay@kneescrapers.com"
 
-  def comment_added
-    mail(to: "b.obrien@me.com", subject: "A comment has been added to the tour you created.")
+  def comment_added(comment)
+    @tour = comment.tour
+    @tour_owner = @tour.user
+    mail(to: @tour_owner.email, subject: "A comment has been added to your tour: #{@tour.name}")
   end
 end
